c10687df5a551ca308c633e814d18737c66e2379
samdd <sam@samdd.me>
2017-12-02 01:46:09 +0000

    :gem: Release 2.0

--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ const pretty						= require('prettysize')
 const chalk 						= require('chalk')
 
 let state = {}
+let defaultMenu = Menu.buildFromTemplate([
+	{
+		label: 'Exit',
+		click: () => {
+			app.quit()
+		}
+	}
+])
+let menu = defaultMenu
 
 
 process.on('unhandledRejection', (reason, p) => {
@@ -20,7 +29,14 @@ process.on('unhandledRejection', (reason, p) => {
 })
 
 app.on('ready', () => {
-	let appIcon = new Tray(path.join(__dirname, './icons/loading.ico'))
+	let tray = new Tray(path.join(__dirname, './icons/tray/loading.ico'))
+	tray.on('double-click', () => {
+		shell.openExternal("http://192.168.1.1/html/statistic.html")
+	})
+	tray.on('click', () => {
+		tray.popUpContextMenu(menu)
+	})
+	tray.setContextMenu(defaultMenu)
 
 	let setCookie = () => {
 		return new Promise((resolve, reject) => {
@@ -69,67 +85,81 @@ app.on('ready', () => {
 					let users = ' user',
 						sms = ''
 					if (notifications.UnreadMessage[0] !== '0') {
-						sms = '(' + notifications.UnreadMessage[0] + ' new messages) '
-						if (notifications.UnreadMessage[0] == '1') sms = '(' + notifications.UnreadMessage[0] + ' new message) '
+						let message = 'message'
+						if (notifications.UnreadMessage[0] !== '1') message += 's'
+
+						sms = '(' + notifications.UnreadMessage[0] + ' new ' + message + ')\n'
 					}
 					if (status.CurrentWifiUser[0] !== 1) users += 's'
-					appIcon.setToolTip(
+					tray.setToolTip(
 						sms +
-						getNetworkType(status.CurrentNetworkType[0]) +
-						' - ' +
-						status.CurrentWifiUser[0] +
-						users +
-						' connected (' +
-						status.BatteryPercent[0] +
-						'%)'
+						getNetworkType(status.CurrentNetworkType[0]) + " - " + status.SignalIcon[0] + " bars\n" +
+						status.CurrentWifiUser[0] + users + ' connected\n' +
+						status.BatteryPercent[0] + '% battery\n' + 
+						pretty(statistics.CurrentDownload[0]) + ' download / ' + pretty(statistics.CurrentUpload[0]) + ' upload'
 					)
-					let signalIcon = 'signal-' + status.SignalIcon[0] + '.ico'
-					if (status.RoamingStatus[0] !== '0') signalIcon = 'roaming-' + status.SignalIcon[0] + '.ico'
-					if (status.ConnectionStatus[0] == '50' || status.ConnectionStatus[0] == '33') signalIcon = 'disabled-' + data.SignalIcon[0] + '.ico'
-					if (status.ConnectionStatus[0] == '900') signalIcon = 'loading.ico'
-					if (notifications.UnreadMessage[0] !== '0') signalIcon = 'sms.ico'
-					appIcon.setImage(
-						path.join(__dirname, './icons/' + signalIcon)
+					let signalIcon = 'signal-' + status.SignalIcon[0]
+					if (status.RoamingStatus[0] !== '0') signalIcon = 'roaming-' + status.SignalIcon[0]
+					if (status.ConnectionStatus[0] == '50' || status.ConnectionStatus[0] == '33') signalIcon = 'disabled-' + data.SignalIcon[0]
+					if (status.ConnectionStatus[0] == '900') signalIcon = 'loading'
+					if (notifications.UnreadMessage[0] !== '0') signalIcon = 'sms'
+					tray.setImage(
+						path.join(__dirname, './icons/tray/' + signalIcon + '.ico')
 					)
-					appIcon.setContextMenu(
-						Menu.buildFromTemplate([
-							{
-								label: 'Connected to ' + getNetworkType(status.CurrentNetworkType[0]) + ' network',
-								click: () => {
-									shell.openExternal("http://192.168.1.1/html/mobilenetworksettings.html")
-								}
-							},
-							{
-								label: status.CurrentWifiUser[0] + users + ' connected',
-								click: () => {
-									shell.openExternal("http://192.168.1.1/html/statistic.html")
-								}
-							},
-							{
-								label: pretty(statistics.CurrentDownload[0]) + ' download / ' + pretty(statistics.CurrentUpload[0]) + ' upload',
-								click: () => {
-									shell.openExternal("http://192.168.1.1/html/statistic.html")
-								}
-							},
-							{
-								label: status.BatteryPercent[0] + '% battery remaining',
-								click: () => {
-									shell.openExternal("http://192.168.1.1/")
-								}
-							},
-							{
-								label: 'Exit',
-								click: () => {
-									app.quit()
-								}
+					menu = Menu.buildFromTemplate([
+						{
+							label: 'Network Type',
+							sublabel: getNetworkType(status.CurrentNetworkType[0]),
+							icon: path.join(__dirname, './icons/context-menu/' + signalIcon + '.png'),
+							click: () => {
+								shell.openExternal("http://192.168.1.1/html/mobilenetworksettings.html")
 							}
-						])
-					)
+						},
+						{
+							label: 'Connected devices',
+							sublabel: status.CurrentWifiUser[0],
+							icon: path.join(__dirname, './icons/context-menu/devices.png'),
+							click: () => {
+								shell.openExternal("http://192.168.1.1/html/statistic.html")
+							}
+						},
+						{
+							label: 'Battery percentage',
+							sublabel: status.BatteryPercent[0] + '%',
+							click: () => {
+								shell.openExternal("http://192.168.1.1/")
+							}
+						},
+						{
+							type: 'separator'
+						},
+						{
+							label: 'Current download / speed',
+							sublabel: pretty(statistics.CurrentDownload[0]) + ' / ' + pretty(statistics.CurrentDownloadRate[0]) + '/s',
+							icon: path.join(__dirname, './icons/context-menu/download.png'),
+							click: () => {
+								shell.openExternal("http://192.168.1.1/html/statistic.html")
+							}
+						},
+						{
+							label: 'Current upload / speed',
+							sublabel: pretty(statistics.CurrentUpload[0]) + ' / ' + pretty(statistics.CurrentUploadRate[0]) + '/s',
+							icon: path.join(__dirname, './icons/context-menu/upload.png'),
+							click: () => {
+								shell.openExternal("http://192.168.1.1/html/statistic.html")
+							}
+						}
+					])
 					
 				}
 				if (state.status && state.notifications && state.statistics) {
 					if (notifications.UnreadMessage[0] > state.notifications.UnreadMessage[0]) {
-						// New SMS
+						let message = "message"
+						if (notifications.UnreadMessage[0] == "1") message += "s"
+						tray.displayBalloon({
+							title: "New SMS received",
+							content: "You have " + notifications.UnreadMessage[0] + " unread " + message
+						})
 					}
 				}
 				state = {
